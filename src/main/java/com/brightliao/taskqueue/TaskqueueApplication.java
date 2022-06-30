0d88aa1ff37686e8e87539833028278f91262182
gmlove <gracekinglau@gmail.com>
2022-06-30 09:33:04 +0800

    basic code framework

---
+++ src/main/java/com/brightliao/taskqueue/TaskqueueApplication.java
@@ -0,0 +1,13 @@
+package com.brightliao.taskqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brightliao/taskqueue/TaskqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightliao.taskqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
